ff9edf45878ccc0957bb2e867f75ccd7785cea50
Isabela Cristina <isabelacristina20005@gmail.com>
2022-07-03 16:22:38 -0300

    Atividades para prova

---
+++ Atividade2/src/br/edu/iftm/exercícios/Exercício7.java
@@ -0,0 +1,41 @@
+package br.edu.iftm.exercícios;
+
+import java.util.Scanner;
+
+public class Exercício7 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Digite a quantidade de numero a ser digitado:");
+        int tam = entrada.nextInt();
+
+        int[] vetor = new int[tam];
+
+        float media = 0.0f;
+        int contadorM = 0;
+        int soma = 0;
+        int numero = 0;
+
+        for (int i = 0; i < tam ; i++) {
+            do {
+                System.out.println("Digite num numero inteiro e positivo");
+                numero = entrada.nextInt();
+
+                if (numero < 0) {
+                    System.out.println("Numero digitado é negativo! Digite um numero positivo");
+                }
+            }while (numero < 0);
+
+            if(numero >= 0){
+                vetor[i] = numero;
+                if(vetor[i] % 3 == 0){
+                    soma = soma+vetor[i];
+
+                    contadorM = contadorM + 1;
+                }
+            }
+        }
+        media = ((float) soma)/contadorM;
+        System.out.println("O valor da média é: " + media);
+    }
+}
